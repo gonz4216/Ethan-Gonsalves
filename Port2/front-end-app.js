3294572bbfb90ea38d3ddc0df29504c8df35ba05
gonz4216 <ethgon19@gmail.com>
2022-07-26 20:34:00 -0400

    main

--- Port2/front-end-app.js
+++ Port2/front-end-app.js
@@ -28,7 +28,10 @@ let isModalOpen = false;
 function toggleModal() {
     if (isModalOpen) {
         isModalOpen = false
+        
+
         return document.body.classList.remove("modal-open")
+       
     }
     isModalOpen = true;
     document.body.classList += " modal-open"
